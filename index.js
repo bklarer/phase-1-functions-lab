5de8b8cdf7cfdeb8da71948c0e4c5d0b277aebf3
Benjamin Klarer <benjaminklarer@gmail.com>
2022-05-19 19:44:23 -0700

    commit tree 7e87530002000863f46e541b5e28cdafbb0f22a2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    if(blocks > 42) {
+        return blocks - 42
+    } else {
+        return (blocks - 42) * -1
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if((start - destination) >= 0) {
+        return ((start - destination) * 264)
+    } else {
+        return ((start - destination) * -264)
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    } else if (distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+
+}
